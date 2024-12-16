7edd788c77d00f90b4c1b90ba022b9debcad9222
SaidurIUT <saidurrahman@iut-dhaka.edu>
2024-12-16 18:45:15 +0600

    KeyCloak authentication in ApiGateway implemented

--- backend/UserServer/src/main/java/com/metahive/UserServer/controllers/UserController.java
+++ backend/UserServer/src/main/java/com/metahive/UserServer/controllers/UserController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/test")
+    @GetMapping("/public/test")
     public ResponseEntity<String> test(){
 
-        return ResponseEntity.ok("Peekaboo from user server !!! ");
+        return ResponseEntity.ok("Peekaboo from user server public test !!! ");
+    }
+
+    @GetMapping("/private/test")
+    public ResponseEntity<String> privateTest(){
+        return ResponseEntity.ok("Peekaboo from user server private test !!! ");
     }
 
 }
