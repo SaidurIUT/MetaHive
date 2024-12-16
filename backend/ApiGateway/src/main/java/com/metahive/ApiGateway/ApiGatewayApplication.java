24b0d3a4a91a183bdf4bfe1aa6360d68bccbe608
SaidurIUT <saidurrahman@iut-dhaka.edu>
2024-12-16 14:59:47 +0600

    Working with Api Gateway and authentication

---
+++ backend/ApiGateway/src/main/java/com/metahive/ApiGateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.metahive.ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiGatewayApplication.class, args);
+
+		System.out.println("Api Gateway Started !!! ");
+	}
+
+}
